39eff3e7033b93cfe53ff3706c1eadf1cad5d73b
Filipe <filipepgm@hotmail.com>
2018-09-29 06:57:56 +0100

    back: add all base endpoints

--- zebra_backend/main.py
+++ zebra_backend/main.py
@@ -24,6 +24,12 @@ class DatabaseClass:
     def __init__(self):
         self.users = dict()
 
+    def getUser(self, key):
+        if not key in self.users:
+            return None
+        else:
+            return self.users[key]
+
 db = DatabaseClass()
 
 
@@ -33,8 +39,71 @@ db = DatabaseClass()
 def ep_hello():
     #return app.send_static_file('index.html')
     return 'Hello, World!'
+    
+@app.route('/get/', methods=['POST'])
+def get_tasks():
+    input = json.loads(request.data)
+    user = db.getUser(input.user)
+    if user:
+        past, future = user.get_schedule()
+        return json.dumps([ [t.json() for t in past], [t.json() for t in future]], indent=4, sort_keys=True, default=str)
+    else:
+        print ("WARNING: user not found")
+
+@app.route('/add/', methods=['POST'])
+def add_task():
+    input = json.loads(request.data)
+    user = db.getUser(input.user)
+    if user:
+        duration = datetime.combine(date.today(), time.fromisoformat(input.duration))  - datetime.combine(date.today(), time())
+        #deadline = datetime.strptime("2007-03-04T21:08:12", "%Y-%m-%dT%H:%M:%S")
+        if input.deadline:
+            deadline = datetime.strptime(input.deadline, "%Y-%m-%dT%H:%M:%S")
+        else:
+            deadline = None
+        task = Task(input.name, duration, deadline, input.notes)
+        user.add_task(task, datetime.now())
+    else:
+        print ("WARNING: user not found")
+
+@app.route('/edit/', methods=['POST'])
+def edit_task():
+    input = json.loads(request.data)
+    user = db.getUser(input.user)
+    if user:
+        task_id = int(input.task_id)
+        duration = datetime.combine(date.today(), time.fromisoformat(input.duration))  - datetime.combine(date.today(), time())
+        #deadline = datetime.strptime("2007-03-04T21:08:12", "%Y-%m-%dT%H:%M:%S")
+        if input.deadline:
+            deadline = datetime.strptime(input.deadline, "%Y-%m-%dT%H:%M:%S")
+        else:
+            deadline = None
+        schedule = datetime.strptime(input.deadline, "%Y-%m-%dT%H:%M:%S")
+        user.edit_task(task_id, input.name, duration, deadline, input.notes, schedule, datetime.now())
+    else:
+        print ("WARNING: user not found")
+
+@app.route('/delete/', methods=['POST'])
+def delete_task():
+    input = json.loads(request.data)
+    user = db.getUser(input.user)
+    if user:
+        task_id = int(input.task_id)
+        user.delete_task(task_id, datetime.now())
+    else:
+        print ("WARNING: user not found")
 
 
+@app.route('/complete/', methods=['POST'])
+def complete_task():
+    input = json.loads(request.data)
+    user = db.getUser(input.user)
+    if user:
+        task_id = int(input.task_id)
+        time_taken = time.fromisoformat(input.time_taken)
+        user.complete_task(task_id, time_taken)
+    else:
+        print ("WARNING: user not found")
 
 if __name__ == '__main__':
     app.run(host = "127.0.0.1", port = 8080, debug = True)
