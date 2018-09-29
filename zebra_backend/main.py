a1d136e00cb84a077952815449b05e6f9c616488
rjgtav <rjgtav@hotmail.com>
2018-09-29 16:58:07 +0100

    Back: small fixes

--- zebra_backend/main.py
+++ zebra_backend/main.py
@@ -37,7 +37,7 @@ db = DatabaseClass()
 
 
 #Dummy data:
-User(1, "Zebra", "zebra@zebroide.zeb")
+db.addUser(User(1, "Zebra", "zebra@zebroide.zeb"))
 
 ############################################################
 
@@ -49,7 +49,7 @@ def ep_hello():
 @app.route('/get/', methods=['POST'])
 def get_tasks():
     input = json.loads(request.data)
-    user = db.getUser(input.user)
+    user = db.getUser(input['user'])
     if user:
         past, future = user.get_schedule()
         return json.dumps([ [t.getDict() for t in past], [t.getDict() for t in future]], indent=4, sort_keys=True, default=str)
@@ -59,15 +59,15 @@ def get_tasks():
 @app.route('/add/', methods=['POST'])
 def add_task():
     input = json.loads(request.data)
-    user = db.getUser(input.user)
+    user = db.getUser(input['user'])
     if user:
-        duration = datetime.combine(date.today(), time.fromisoformat(input.duration))  - datetime.combine(date.today(), time())
+        duration = datetime.combine(date.today(), time.fromisoformat(input['duration']))  - datetime.combine(date.today(), time())
         #deadline = datetime.strptime("2007-03-04T21:08:12", "%Y-%m-%dT%H:%M:%S")
-        if input.deadline:
-            deadline = datetime.strptime(input.deadline, "%Y-%m-%dT%H:%M:%S")
+        if input['deadline']:
+            deadline = datetime.strptime(input['deadline'], "%Y-%m-%dT%H:%M:%S")
         else:
             deadline = None
-        task = Task(input.name, duration, deadline, input.notes)
+        task = Task(input['name'], duration, deadline, input['notes'])
         user.add_task(task, datetime.now())
     else:
         print ("WARNING: user not found")
@@ -75,26 +75,26 @@ def add_task():
 @app.route('/edit/', methods=['POST'])
 def edit_task():
     input = json.loads(request.data)
-    user = db.getUser(input.user)
+    user = db.getUser(input['user'])
     if user:
-        task_id = int(input.task_id)
-        duration = datetime.combine(date.today(), time.fromisoformat(input.duration))  - datetime.combine(date.today(), time())
+        task_id = int(input['task_id'])
+        duration = datetime.combine(date.today(), time.fromisoformat(input['duration']))  - datetime.combine(date.today(), time())
         #deadline = datetime.strptime("2007-03-04T21:08:12", "%Y-%m-%dT%H:%M:%S")
-        if input.deadline:
-            deadline = datetime.strptime(input.deadline, "%Y-%m-%dT%H:%M:%S")
+        if input['deadline']:
+            deadline = datetime.strptime(input['deadline'], "%Y-%m-%dT%H:%M:%S")
         else:
             deadline = None
-        schedule = datetime.strptime(input.deadline, "%Y-%m-%dT%H:%M:%S")
-        user.edit_task(task_id, input.name, duration, deadline, input.notes, schedule, datetime.now())
+        schedule = datetime.strptime(input['deadline'], "%Y-%m-%dT%H:%M:%S")
+        user.edit_task(task_id, input['name'], duration, deadline, input['notes'], schedule, datetime.now())
     else:
         print ("WARNING: user not found")
 
 @app.route('/delete/', methods=['POST'])
 def delete_task():
     input = json.loads(request.data)
-    user = db.getUser(input.user)
+    user = db.getUser(input['user'])
     if user:
-        task_id = int(input.task_id)
+        task_id = int(input['task_id'])
         user.delete_task(task_id, datetime.now())
     else:
         print ("WARNING: user not found")
@@ -103,10 +103,10 @@ def delete_task():
 @app.route('/complete/', methods=['POST'])
 def complete_task():
     input = json.loads(request.data)
-    user = db.getUser(input.user)
+    user = db.getUser(input['user'])
     if user:
-        task_id = int(input.task_id)
-        time_taken = time.fromisoformat(input.time_taken)
+        task_id = int(input['task_id'])
+        time_taken = time.fromisoformat(input['time_taken'])
         user.complete_task(task_id, time_taken)
     else:
         print ("WARNING: user not found")
@@ -114,9 +114,9 @@ def complete_task():
 @app.route('/uncomplete/', methods=['POST'])
 def uncomplete_task():
     input = json.loads(request.data)
-    user = db.getUser(input.user)
+    user = db.getUser(input['user'])
     if user:
-        task_id = int(input.task_id)
+        task_id = int(input['task_id'])
         user.uncomplete_task(task_id, datetime.now())
     else:
         print ("WARNING: user not found")
@@ -125,9 +125,9 @@ def uncomplete_task():
 @app.route('/switch/', methods=['POST'])
 def switch_task():
     input = json.loads(request.data)
-    user = db.getUser(input.user)
+    user = db.getUser(input['user'])
     if user:
-        task_id = int(input.task_id)
+        task_id = int(input['task_id'])
         user.switch_task(task_id, datetime.now())
     else:
         print ("WARNING: user not found")
@@ -135,9 +135,9 @@ def switch_task():
 @app.route('/postpone/', methods=['POST'])
 def postpone_task():
     input = json.loads(request.data)
-    user = db.getUser(input.user)
+    user = db.getUser(input['user'])
     if user:
-        task_id = int(input.task_id)
+        task_id = int(input['task_id'])
         user.postpone_task(task_id, datetime.now())
     else:
         print ("WARNING: user not found")
