361ed8c9fef0663703d27c709691b54f16ea72f6
Filipe <filipepgm@hotmail.com>
2018-09-29 17:21:20 +0100

    back: add iCal class

--- zebra_backend/main.py
+++ zebra_backend/main.py
@@ -14,11 +14,25 @@ from user import User
 app = Flask(__name__)
 CORS(app)
 
-timestamp = None
+from icalevents.icalevents import events
+class IcalCalendar:
+    def __init__(self, url):
+        self.url = url
+        self.refreshed = None
+    
+    def refresh(self, now):
+        if not self.refreshed or self.refreshed + timedelta(days=1) < now:
+            self.es = events(self.url)
+    
+    def get_busy_times(self, day):
+        res = []
+        for ev in self.es:
+            if ev.start.day() == day:
+                res.append((ev.start.time(), ev.end.time()))
+        return res
 
-class Calendar:
-    def __init__(self):
-        pass
+
+    
 
 class DatabaseClass:
     def __init__(self):
